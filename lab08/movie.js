a9b6a40ec0f839eff09e34c29e7d352da7aadad0
Elijah_Fabon <113316563+Elijah-Fabon@users.noreply.github.com>
2022-11-07 14:46:28 -0800

    Add movie data functions to lab08

---
+++ lab08/movie.js
@@ -0,0 +1,47 @@
+
+
+
+
+setup = function() 
+{
+  $.ajax
+  (
+    {
+      url: "https://api.themoviedb.org/3/movie/top_rated?api_key=8b35647f9448076a6df7d25f874f6d3b&language=en-US&page=1",
+      type: "GET",
+      success: function (data) 
+      {
+        // console.log(data);
+        for (i= 0; i <data.results.length; i++)
+        {
+          $("main").append (
+            `
+            <div>
+            ${data.results[i].title}
+            <p>
+            ${data.results[i].overview}
+            </p>
+            <img src="https://image.tmdb.org/t/p/w300/${data.results[i].poster_path}">
+            <button movieID="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
+            <hr>
+            </div>
+            `
+          )
+        }
+      }
+    }
+  )
+  $("body").on("click", ".backdropBtn", function (){
+    // console.log(`https://image.tmdb.org/t/p/w300/${$(this).attr('movieID')}`)
+    $("aside").html(
+      `
+      <img src="https://image.tmdb.org/t/p/w300/${$(this).attr('movieID')}" id="backdrop">
+      `
+    )
+  })
+}
+
+
+
+
+$(document).ready(setup)
