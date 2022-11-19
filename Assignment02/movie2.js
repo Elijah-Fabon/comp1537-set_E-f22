bbe240b78616d7f8526d823d4caeb2d419d14ecc
Elijah_Fabon <113316563+Elijah-Fabon@users.noreply.github.com>
2022-11-19 13:41:35 -0800

    Add for loop to create page buttons

--- Assignment02/movie2.js
+++ Assignment02/movie2.js
@@ -1,7 +1,5 @@
 var PAGE_SIZE;
-var CURRENT_PAGE = 1;
-
-
+var CURRENT_PAGE;
 
 display_page = () => {
    $.ajax
@@ -13,25 +11,29 @@ display_page = () => {
         {
           let start_index = PAGE_SIZE * (CURRENT_PAGE - 1);
           let end_index = PAGE_SIZE * (CURRENT_PAGE - 1) + PAGE_SIZE;
-          for (i = start_index; i < end_index; i++)
-          // console.log(data);
-          // for (i= 0; i <data.results.length; i++)
-          {
+          for (i = start_index; i < end_index; i++) {
             $("main").append (
               `
               <div>
-              ${data.results[i].title}
-              <p>
-              ${data.results[i].overview}
-              </p>
-              <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
-              style="width: 100%;">
-              <button movieID="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
-              <hr>
+                ${data.results[i].title}
+                <p>
+                  ${data.results[i].overview}
+                </p>
+                <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}"
+                style="width: 100%;">
+                <button movieID="${data.results[i].backdrop_path}" class="backdropBtn"> BackDrop Image </button>
+                <hr>
               </div>
               `
             )
           }
+          for(i = 1; i <= Math.ceil(data.results.length / (PAGE_SIZE)); i++) {
+            $("#pagebuttons").append(
+              `
+              <button value="i" id="pagebtn">${i}</button>
+              `
+            )
+  }
         }
       }
     )
@@ -39,17 +41,28 @@ display_page = () => {
 
 setup = function() {
   PAGE_SIZE = $("#pagesizemenu option:selected").val();
+
   $("#searchbtn").click(() => {
+    $("main").empty()
+    $("aside").empty()
+    $("#pagebuttons").empty()
     display_page()
   })
 
-  $("pagesizemenu").change(() => {
+  $("#pagesizemenu").change(() => {
     // PAGE_SIZE = $("#pagesizemenu").val();
     PAGE_SIZE = $("#pagesizemenu option:selected").val();
+    
     console.log("PAGESIZE", PAGE_SIZE)
   })
 
-
+  $("body").on("click", "#pagebtn", function () {
+    CURRENT_PAGE = $("#pagebtn").val();
+    $("main").empty()
+    $("aside").empty()
+    $("#pagebuttons").empty()
+    display_page()
+  })
 
   $("body").on("click", ".backdropBtn", function (){
     // console.log(`https://image.tmdb.org/t/p/w300/${$(this).attr('movieID')}`)
